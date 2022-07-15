05eb71afdfaef256c43cee0ba392279efce9130c
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-15 18:20:51 +0000

    Added Loops

---
+++ Loops/Concepts/ForLoop.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+import java.util.Scanner;
+
+// Prints 1-10
+public class ForLoop{
+  public static void main (String[] args){
+    for (int i = 1; i <=10; i++){
+      System.out.println(i);
+    }
+  }
+}
