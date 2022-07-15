67adbeea7aa529c1979c75997037bbb34ba9ad13
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-15 18:12:33 +0000

    Added Loops

---
+++ Loops/Concepts/DoWhile.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.Scanner;
+import java.util.*;
+
+// Print 1-10
+public class DoWhile{
+  public static void main(String[] args){
+    int i = 0;
+    do{
+      i++;
+      System.out.println(i);
+    } while (i < 10);
+  }
+}
---
+++ Loops/Concepts/While.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.Scanner;
+import java.util.*;
+
+public class While {
+  public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+    int a = 1;
+    while(a < 5){
+      System.out.println(a);
+      a++;
+    }
+  }
+}
