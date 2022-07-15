3bf1d2e13e6b928e98775bed8bec6756e2e59423
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-15 18:27:46 +0000

    Fixed Error

--- Loops/Concepts/While.java
+++ Loops/Concepts/While.java
@@ -2,11 +2,12 @@ import java.lang.*;
 import java.util.Scanner;
 import java.util.*;
 
+// Print 1-10
 public class While {
   public static void main (String[] args){
     Scanner sc = new Scanner (System.in);
     int a = 1;
-    while(a < 5){
+    while(a <= 10){
       System.out.println(a);
       a++;
     }
