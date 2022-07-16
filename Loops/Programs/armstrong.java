aba66925e330f7695c034992d86fc9144dd09105
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-16 18:22:48 +0000

    Armstrong number

--- Loops/Programs/armstrong.java
+++ Loops/Programs/armstrong.java
@@ -1,11 +1,17 @@
 import java.lang.*;
 import java.util.Scanner;
 
-public class countDigits{
+public class armstrong{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the number: ");
     int n = sc.nextInt();
-    
+    int r, arm = 0;
+    while(n > 0){
+      r = n % 10;
+      n = n / 10;
+      arm = arm + r * r * r;
+      System.out.println("The armstrong number is" + arm);
+    }
   }
 }
