6c1ade95c2b75fd211dee3ff38ff7500c306d3ef
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-18 17:39:13 +0000

    nestedFor Sample

--- Loops/Concepts/nestedFor.java
+++ Loops/Concepts/nestedFor.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class nestedFor{
   public static void main(String[] args){
     Scanner sc = new Scanner (System.in);
-    
+    for(int i = 0; i <= n; i++){
+      System.out.println("Content");
+      for(int j = 0; j <= i; j++)
+        System.out.println("Content");
+    }
   }
 }
