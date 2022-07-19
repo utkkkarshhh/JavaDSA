072eb397743842a629e46b12fd3553ccb1b84255
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-19 18:12:11 +0000

    out.format

--- Loops/Programs/pattern.java
+++ Loops/Programs/pattern.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 
 public class pattern{
   public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
+    // With Counter
+    int count = 0;
+    for (int i = 1; i <= 5; i++){
+      for (int j = 1; j <= 5; j++){
+        System.out.format("%02d  ", ++count);
+      }
+      System.out.println("");
+    }
     
   }
 }
