a672735eee1e1a2ce3b02f04613f0e25ca8f81f2
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-20 13:14:06 +0000

    starLoops

---
+++ Loops/Programs/starLoops.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class pattern4{
+  public static void main(String[] args){
+    for (int i = 1; i <= 5; i++){
+      for (int j = 1; j <= 5 - i + 1; j++){
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+    
+  }
+}
