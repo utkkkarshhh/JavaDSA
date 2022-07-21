3e12efaa04545ad506a363305109aced4345881b
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-21 16:31:07 +0000

    added star loop with for loop

--- Loops/Programs/starLoops.java
+++ Loops/Programs/starLoops.java
@@ -1,14 +1,18 @@
 import java.lang.*;
 import java.util.Scanner;
 
-public class pattern4{
+public class starLoops{
   public static void main(String[] args){
-    for (int i = 1; i <= 5; i++){
-      for (int j = 1; j <= 5 - i + 1; j++){
-        System.out.print("*");
+    for(int i=1; i<=5; i++){
+      for(int j=1; j<=5; j++){
+        if(i<=j){
+          System.out.print("* ");
+        }
+        else {
+          System.out.print("");
+        }
       }
-      System.out.println("");
+      System.out.println(" ");
     }
-    
   }
 }
