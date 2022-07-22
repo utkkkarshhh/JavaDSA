33689e6a6f266a0ab1e63e9b8949eea7c536517c
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-22 15:53:14 +0000

    fixed starLoops program

--- Loops/Programs/starLoops.java
+++ Loops/Programs/starLoops.java
@@ -1,18 +1,18 @@
 import java.lang.*;
 import java.util.Scanner;
 
-public class starLoops{
-  public static void main(String[] args){
-    for(int i=1; i<=5; i++){
-      for(int j=1; j<=5; j++){
-        if(i<=j){
-          System.out.print("* ");
-        }
-        else {
-          System.out.print("");
-        }
-      }
-      System.out.println(" ");
-    }
-  }
+public class starLoops
+{
+	public static void main(String[] args) {
+		for(int i = 1; i<=5; i++){
+		    for(int j = 1; j<=5; j++){
+		        if(j>=i){
+		            System.out.print("* ");
+		        }
+		        else
+		        System.out.print("  ");
+		    }
+		    System.out.println(" ");
+		}
+	}
 }
