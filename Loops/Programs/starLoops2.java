375fcbf6ad06e2abbdb99c1d2ba06cc09059f611
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-22 15:58:34 +0000

    new star pattern loop

---
+++ Loops/Programs/starLoops2.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class starLoops2{
+  public static void main(String[] args){
+    for(int i = 1; i<=5; i++){
+      for(int j = 1; j<=5; j++){
+        if(i+j > 5){
+          System.out.print("* ");
+        }
+        else
+          System.out.print("  ");
+      }
+      System.out.println(" ");
+    }
+  }
+}
