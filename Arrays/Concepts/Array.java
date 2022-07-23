4993d909324604251dea6ce5e5f70beba9aeccef
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-23 18:36:27 +0000

    Printing an entire array

---
+++ Arrays/Concepts/Array.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Array{
+  public static void main(String[] args){
+    int A[] = {10, 20, 30, 40, 50};
+    System.out.println(A[0]);
+  }
+}
