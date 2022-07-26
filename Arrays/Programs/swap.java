e85e7369ae56e0b34dc89f92dc13683feb5dd393
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-26 17:28:52 +0000

    modified program

--- Arrays/Programs/swap.java
+++ Arrays/Programs/swap.java
@@ -5,12 +5,14 @@ public class swap{
   public static void main(String[] args) {
     int S [] = {2, 1, 9, 3, 10, 15};
 
+    // Swapping Operation
     for(int i = 0; i < S.length; i += 2){
       int temp = S[i];
       S[i] = S [i + 1];
       S [i + 1] = temp;
     }
-    
+
+    // Printing the Swapped Array
     for(int i = 0; i < S.length; i++){
       System.out.print(S[i] + ", ");
     }
