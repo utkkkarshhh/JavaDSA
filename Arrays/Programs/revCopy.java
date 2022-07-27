8ddf50a15f82201bc763da46c0fd503c0f287a8d
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-27 23:02:30 +0000

    reverse copy an array

--- Arrays/Programs/revCopy.java
+++ Arrays/Programs/revCopy.java
@@ -1,19 +1,19 @@
 import java.lang.*;
 import java.util.Scanner;
 
-public class copying{
+public class revCopy{
   public static void main(String[] args){
     int A [] = {10, 20, 30, 40, 50};
     int B [] = new int [A.length];
     
     // Traversing the array array
     for(int i = 0; i < A.length; i++){
-        B[i] = A[i];
+        B[B.length - i - 1] = A[i];
     }
 
     // Printing the array
-    for(int i = 0; i <A.length; i++){
-      System.out.println(B[i]);
+    for(int i = 0; i <B.length; i++){
+      System.out.println("Index Number : " + i + " = " + B[i]);
     }
   }
 }
