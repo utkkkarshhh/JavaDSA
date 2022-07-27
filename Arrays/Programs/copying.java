3f5bf417203e7aa26bd5afce78af158622276acc
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-27 22:42:53 +0000

    copying and deletion inside array

---
+++ Arrays/Programs/copying.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    int A [] = {10, 20, 30, 40, 50};
+    int B [] = new int [A.length];
+    
+    // Traversing the array array
+    for(int i = 0; i < A.length; i++){
+        B[i] = A[i];
+    }
+
+    // Printing the array
+    for(int i = 0; i <A.length; i++){
+      System.out.println(B[i]);
+    }
+  }
+}
--- Arrays/Programs/deletion.java
+++ Arrays/Programs/deletion.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class deletion{
   public static void main(String[] args){
       int A[] = {5, 9, 6, 10, 12, 17};
+      
+    //   Taking the element user wants to delete
+        Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number you want to remove: ");
+    int n = sc.nextInt();
+      
     //   Deleting the element from array
-    int n = 9;
     for(int i = 0; i < A.length; i++){
         if(A[i] == n){
-            for(int j = i; j < A.length; j++){
+            for(int j = i; j < A.length - 1; j++){
                 A[j] = A[j + 1];
             }
             break;
@@ -15,7 +20,7 @@ public class deletion{
     }
       
     //   Printing the new array
-      for(int i = 0; i < A.length; i++){
+      for(int i = 0; i < A.length - 1; i++){
         System.out.print(A[i] + " ");
       }
   }
