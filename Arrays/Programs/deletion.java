0101cc0918859f6cd39ef808cc45abac5697809e
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-27 18:59:18 +0000

    delete element from array

--- Arrays/Programs/deletion.java
+++ Arrays/Programs/deletion.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class deletion{
   public static void main(String[] args){
-    
+      int A[] = {5, 9, 6, 10, 12, 17};
+    //   Deleting the element from array
+    int n = 9;
+    for(int i = 0; i < A.length; i++){
+        if(A[i] == n){
+            for(int j = i; j < A.length; j++){
+                A[j] = A[j + 1];
+            }
+            break;
+        }
+    }
+      
+    //   Printing the new array
+      for(int i = 0; i < A.length; i++){
+        System.out.print(A[i] + " ");
+      }
   }
 }
