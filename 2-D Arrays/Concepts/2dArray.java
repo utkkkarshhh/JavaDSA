45955789baf4cbda2e12449ab05762dd9bed29e1
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-31 17:16:11 +0000

    2D array concepts

--- 2-D Arrays/Concepts/2dArray.java
+++ 2-D Arrays/Concepts/2dArray.java
@@ -4,5 +4,7 @@ import java.util.Scanner;
 
 public class 2dArray{
   public static void main(String[] args){
+    int A[][] = new int [3][4]; //Standard Declaration
+    int A[][] = {{10, 20, 30},{40, 50, 60},{70, 80, 90}}; //Direct Initialization
   }
 }
