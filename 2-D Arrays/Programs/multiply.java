f1c575da449e0c9dc011b9fdc901df98ebc05bd7
Utkarsh Bhardwaj <67866657+utkkkarshhh@users.noreply.github.com>
2022-07-31 18:12:47 +0000

    progs

---
+++ 2-D Arrays/Programs/multiply.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class multiply{
+  public static void main(String[] args){
+  int A[][] = {{3, 5, 9},{7, 6, 2},{4, 3, 5}}; 
+  int B[][] = {{1, 5, 2},{6, 8, 4},{3, 9, 7}};
+  int C[][] = new int[3][3];
+
+  // Traversing through C and adding values.
+    for(int i=0; i<C.length; i++){
+      for(int j=0; j<A[0].length; j++){
+        C[i][j] = A[i][j] * B[i][j];
+      }
+    }
+
+    // Printing C Array
+    for(int i=0; i<C.length; i++){
+      for (int j=0; j<C[0].length; j++){
+        System.out.print(C[i][j] + " ");
+      }
+      System.out.print("\n");
+    }
+  }
+}
