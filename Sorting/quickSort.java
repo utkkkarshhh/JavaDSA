5df6e41015780b69e2ef24ff15e8881140a644cd
Utkarsh Bhardwaj <utkarshbhardwaj@Utkarshs-MacBook-Air.local>
2024-01-23 16:12:16 +0530

    added more programs

--- Sorting/quickSort.java
+++ Sorting/quickSort.java
@@ -1,10 +1,52 @@
-public class quickSort{
-    public static void quickSort(){
-        System.out.println("Hello World");
+public class QuickSort {
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(array, low, high); // Pivot is created and placed at its right position!
+            quickSort(array, low, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, high);
+        }
     }
 
-    public static void main(String [] args){
-        quickSort();
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high - 1 && array[i] <= pivot) {  // Fix array index out of bounds
+                i++;
+            }
+            while (j >= low + 1 && array[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, low, j);
+        return j;
     }
 
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 60, 50, 20, 30, 40};
+        System.out.println("Unsorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
